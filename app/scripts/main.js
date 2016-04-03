0a7af00a9e6a8dd7c3c769e675f5d9826ff0868d
jonathanlie94 <jonathanlie94@gmail.com>
2016-04-03 15:40:54 +0800

    Display list of places nearby the user's current location

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,85 @@
 import ko from 'knockout';
-import Location from 'models/location';
-import Global from './global';
-
-// Dummy data
-let data = [{
-  lat: 1.00,
-  lng: 1.20,
-  name: "Some random name"
-}, {
-  lat: 1.20,
-  lng: 1.40,
-  name: "Another random name"
-}];
+import Place from 'models/place';
 
 class ViewModel {
-  constructor(locations) {
-    this.locations = ko.observableArray(locations.map((item) => {
-      return new Location(item.lat, item.lng, item.name);
-    }));
-    this.keyword = ko.observable('');
+  constructor() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10
+    });
+    this._browserSupportFlag = false;
+    this._initialLocation = null;
+    this.places = ko.observableArray();
+    this.selectedPlace = ko.observable("");
+    this.defaultLocations = {
+      siberia: new google.maps.LatLng(60, 105),
+      newyork: new google.maps.LatLng(40.69847032728747, -73.9514422416687)
+    };
+    this.getCurrentLocation();
+    this.keyword = ko.observable(''); // Search keyword
   }
+
+  getCurrentLocation() {
+    // Try W3C Geolocation
+    if (navigator.geolocation) {
+      this._browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition((position) => {
+          this._initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          this.map.setCenter(this._initialLocation);
+          this.requestPlaces();
+        }, () => this._handleNoGeolocation(this._browserSupportFlag)
+      );
+    }
+    // Browser doesn't support Geolocation
+    else {
+      this._browserSupportFlag = false;
+      this._handleNoGeolocation(this._browserSupportFlag);
+    }
+  }
+
+  _handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      this._initialLocation = this.defaultLocations.siberia;
+    }
+    else {
+      this._initialLocation = this.defaultLocations.newyork;
+    }
+    this.map.setCenter(this._initialLocation);
+    this.requestPlaces();
+  }
+
+  requestPlaces() {
+    let service = new google.maps.places.PlacesService(this.map);
+    service.nearbySearch({
+      location: new google.maps.LatLng(
+        this._initialLocation.lat(),
+        this._initialLocation.lng()
+      ),
+      radius: '20000'
+    }, (results, status) => {
+      console.log(status);
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 0; i < results.length; i ++) {
+          console.log(results[i]);
+          let place = new Place(
+            results[i].lat,
+            results[i].lng,
+            results[i].name,
+            results[i].icon,
+            results[i].photos,
+            results[i].types,
+            results[i].rating
+          );
+          console.log(place);
+          this.places.push(place);
+        }
+      }
+      console.log(this.places());
+    });
+  }
+
+
 }
 
 export default ViewModel;
 
-ko.applyBindings(new ViewModel(data));
+ko.applyBindings(new ViewModel());
---
+++ app/scripts/models/place.js
@@ -0,0 +1,16 @@
+import ko from 'knockout';
+
+class Place {
+  constructor(lat, lng, name, icon, photos, types, rating) {
+    this.lat = ko.observable(lat);
+    this.lng = ko.observable(lng);
+    this.name = ko.observable(name);
+    this.icon = ko.observable(icon);
+    this.photos = ko.observable(photos);
+    this.types = ko.observable(types);
+    this.rating = ko.observable(rating);
+    this.visible = ko.observable(true);
+  }
+}
+
+export default Place;
